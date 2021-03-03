758a992d9560f61a886ef573bd2a8788d48b2572
unknown <wahid.warid55@gmail.com>
2021-03-03 08:44:36 +0600

    Pages URL added

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ app.set("port", process.env.PORT || 3000)
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
+app.get('/about-us', function(req, res) {
+    res.render('pages/common')
+})
+app.get('/products/:slug', function(req, res) {
+    res.render('pages/common')
+})
+app.get('/blog', function(req, res) {
+    res.render('pages/common')
+})
+app.get('/services/:slug', function(req, res) {
+    res.render('pages/common')
+})
+app.get('/join-us', function(req, res) {
+    res.render('pages/common')
+})
+app.get('/faq', function(req, res) {
+    res.render('pages/common')
+})
+app.get('/contact-us', function(req, res) {
+    res.render('pages/common')
+})
+
 app.get('/', function(req, res) {
     res.render('pages/index')
 })
